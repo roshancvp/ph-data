765c5a2a541be0d63a4ea09bfacd08525cfc05a5
Roshan Prabhakar <roshancvp@gmail.com>
2018-01-12 23:27:48 -0800

    clean dataset

---
+++ clean.py
@@ -0,0 +1,16 @@
+import csv
+import numpy as np
+
+def clean():
+    with open('test.csv') as raw:
+        reader = csv.reader(raw)
+        write(list(reader), 'test_clean.csv')
+
+def write(reader, out):
+    my_file = open(out, 'w')
+    with my_file:
+        writer = csv.writer(my_file)
+        writer.writerows(np.array(reader)[1:1000,[-2,-1]])
+
+if __name__ == '__main__':
+    clean()
